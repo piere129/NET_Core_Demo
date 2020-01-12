cef82a0e28a40d2cfd95dab55a74932dd07792ee
Pieter Desseyn <Pieter.Desseyn@codit.eu>
2020-01-12 13:35:27 +0100

    added crud for child

---
+++ NetCore_Demo/Controllers/ChildController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LibraryData;
+using LibraryData.Models;
+
+namespace NetCore_Demo.Controllers
+{
+    public class ChildController : Controller
+    {
+        private readonly LibraryContext _context;
+
+        public ChildController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Child
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.children.ToListAsync());
+        }
+
+        // GET: Child/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var child = await _context.children
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (child == null)
+            {
+                return NotFound();
+            }
+
+            return View(child);
+        }
+
+        // GET: Child/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Child/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Address,TelephoneNumber")] Child child)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(child);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(child);
+        }
+
+        // GET: Child/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var child = await _context.children.FindAsync(id);
+            if (child == null)
+            {
+                return NotFound();
+            }
+            return View(child);
+        }
+
+        // POST: Child/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Address,TelephoneNumber")] Child child)
+        {
+            if (id != child.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(child);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ChildExists(child.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(child);
+        }
+
+        // GET: Child/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var child = await _context.children
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (child == null)
+            {
+                return NotFound();
+            }
+
+            return View(child);
+        }
+
+        // POST: Child/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var child = await _context.children.FindAsync(id);
+            _context.children.Remove(child);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ChildExists(int id) 
+        {
+            return _context.children.Any(e => e.Id == id);
+        }
+    }
+}
